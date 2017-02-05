0df6e8b842a95c010acd19638a2188e1aa65e003
Bhuamik <bhaumik.shah@indianic.com>
2017-02-05 13:41:41 +0530

    Tid Master

--- src/java/sales/GetSalesDetail.java
+++ src/java/sales/GetSalesDetail.java
@@ -54,9 +54,13 @@ public class GetSalesDetail extends HttpServlet {
         if (dataConnection != null) {
             try {
                 String sql = "select l.ac_cd,l.REF_NO,l.INV_NO,l.V_TYPE,concat(a.FNAME,' ',a.MNAME,' ',a.LNAME) as ac_name,l.V_DATE,l.NET_AMT,"
-                        + " CASH_AMT,BANK_AMT,CARD_AMT,BAJAJ_AMT,SFID,REMARK "
+                        + " CASH_AMT,BANK_AMT,CARD_AMT,BAJAJ_AMT,SFID,REMARK,l.branch_cd "
                         + " from VILSHD l left join ACNTMST a on l.AC_CD=a.AC_CD left join payment p on l.ref_no=p.ref_no where v_date>=? and v_date<=? "
-                        + "and v_type=? and is_del=0 and l.branch_cd=" + branch_cd + " order by INV_NO,v_date";
+                        + "and v_type=? and is_del=0 ";
+                if (!branch_cd.equalsIgnoreCase("0")) {
+                    sql += " and l.branch_cd=" + branch_cd;
+                }
+                sql += " order by INV_NO,v_date";
                 PreparedStatement pstLocal = dataConnection.prepareStatement(sql);
                 pstLocal.setString(1, from_date);
                 pstLocal.setString(2, to_date);
@@ -77,6 +81,7 @@ public class GetSalesDetail extends HttpServlet {
                     object.addProperty("BAJAJ_AMT", rsLocal.getDouble("BAJAJ_AMT"));
                     object.addProperty("SFID", rsLocal.getString("SFID"));
                     object.addProperty("AC_CD", rsLocal.getString("AC_CD"));
+                    object.addProperty("BRANCH_CD", rsLocal.getString("BRANCH_CD"));
                     array.add(object);
                 }
 //                response.getWriter().print(array.toString());
